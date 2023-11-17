b83ba83d230c0907da9c5b4903cb00edb4766fbb
lieuannhieua7 <150890797+lieuannhieua7@users.noreply.github.com>
2023-11-17 21:04:09 +0700

    Add files via upload

---
+++ gioithieu/Giới thiệu.bat
@@ -0,0 +1,14 @@
+@echo off
+title Giới thiệu (by VFCMIC, Khang, LieuAnNhieu)
+echo Tat ca app khac trong trong thu muc nay deu do VFCMIC, Khang, LieuAnNhieu.
+pause
+echo Lien he voi chung toi: vfcmicofficial@gmail.com, lieuannhieukhang@gmail.com
+pause
+echo Cam on vi da su dung.
+echo An nut bat ky de nhan cac thong tin lien he cua chung toi!
+pause
+start chrome.exe "facebook.com/vfcmic"
+start chrome.exe "lieuannhieua7.github.io"
+echo He thong: An nut bat ky de thoat /*_*\
+pause
+exit
